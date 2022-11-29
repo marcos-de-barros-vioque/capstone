efb28af2953ce24ddff20c17ce4053b48286cffb
Marcos de Barros Vioque <marcos.debarrosvioque@gmail.com>
2022-11-29 15:36:00 +0100

    added some fixes in order the test to pass

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -2,7 +2,7 @@ import {render, screen} from "@testing-library/react";
 import Navbar from "../components/Layout";
 import "@testing-library/jest-dom";
 
-describe("Navbar", () => {
+describe("Home", () => {
   it("checks position of navigation", () => {
     render(<Navbar />);
 
--- components/Navbar.js
+++ components/Navbar.js
@@ -1,67 +1,67 @@
-import styled from "styled-components";
-import Image from "next/image";
+//import styled from "styled-components";
+//import Image from "next/image";
 
-export function Navbar() {
-  return (
-    <>
-      <NavBar>
-        <NavList>
-          <NavItem>
-            <Image
-              src="/home.png"
-              alt="Home Page Icon"
-              width={40}
-              height={40}
-            />
-          </NavItem>
-          <NavItem>
-            <Image
-              src="/add-spending.png"
-              alt="Add Spendings Page Icon"
-              width={40}
-              height={40}
-            />
-          </NavItem>
-          <NavItem>
-            <Image
-              src="/tips-tricks.png"
-              alt="Tips & Tricks Page Icon"
-              width={40}
-              height={40}
-            />
-          </NavItem>
-          <NavItem>
-            <Image
-              src="/rewards.png"
-              alt="Rewards Page Icon"
-              width={40}
-              height={40}
-            />
-          </NavItem>
-        </NavList>
-      </NavBar>
-    </>
-  );
-}
+//export function Navbar() {
+//  return (
+//    <>
+//      <NavBar>
+//        <NavList>
+//          <NavItem>
+//            <Image
+//              src="/home.png"
+//              alt="Home Page Icon"
+//              width={40}
+//              height={40}
+//            />
+//          </NavItem>
+//          <NavItem>
+//            <Image
+//              src="/add-spending.png"
+//              alt="Add Spendings Page Icon"
+//              width={40}
+//              height={40}
+//            />
+//          </NavItem>
+//          <NavItem>
+//            <Image
+//              src="/tips-tricks.png"
+//              alt="Tips & Tricks Page Icon"
+//              width={40}
+//              height={40}
+//            />
+//          </NavItem>
+//          <NavItem>
+//            <Image
+//              src="/rewards.png"
+//              alt="Rewards Page Icon"
+//              width={40}
+//              height={40}
+//            />
+//          </NavItem>
+//        </NavList>
+//      </NavBar>
+//    </>
+//  );
+//}
 
-const NavBar = styled.nav`
-  border-top: 3px solid black;
-  position: fixed;
-  bottom: 0;
-  width: 100%;
-`;
+//const NavBar = styled.nav`
+//  border-top: 3px solid black;
+//  position: fixed;
+//  bottom: 0;
+//  width: 100%;
+//`;
 
-const NavList = styled.ul`
-  display: flex;
-  justify-content: space-around;
-  align-items: center;
-  list-style: none;
-  margin: 0;
-  padding: 0;
-  width: 100%;
-  height: 10vh;
-`;
+//const NavList = styled.ul`
+//  display: flex;
+//  justify-content: space-around;
+//  align-items: center;
+//  list-style: none;
+//  margin: 0;
+//  padding: 0;
+//  width: 100%;
+//  height: 10vh;
+//`;
 
-const NavItem = styled.li`
-  text-decoration: none;
-`;
+//const NavItem = styled.li`
+//  text-decoration: none;
+//`;
