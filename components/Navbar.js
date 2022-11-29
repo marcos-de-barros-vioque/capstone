c864707e21f0c9660babbdfc93d6e1a4512ea27e
Marcos de Barros Vioque <marcos.debarrosvioque@gmail.com>
2022-11-29 15:08:28 +0100

    removed role at wrong src

--- components/Navbar.js
+++ components/Navbar.js
@@ -4,7 +4,7 @@ import Image from "next/image";
 export function Navbar() {
   return (
     <>
-      <NavBar role="navbar">
+      <NavBar>
         <NavList>
           <NavItem>
             <Image
