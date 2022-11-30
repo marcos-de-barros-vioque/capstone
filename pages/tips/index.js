cdaa1667a51ab638803f6b285c36b98eefa2da1a
Marcos de Barros Vioque <marcos.debarrosvioque@gmail.com>
2022-11-30 15:09:05 +0100

    added initial content to the pages

---
+++ pages/tips/index.js
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+export default function TipsPage() {
+  return (
+    <div>
+      <StyledBody>
+        <p>This page is under construction</p>
+      </StyledBody>
+    </div>
+  );
+}
+
+const StyledBody = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: #006269;
+  border-radius: 10px;
+  width: 90vw;
+  height: 75vh;
+  gap: 20px;
+  margin-top: 10px;
+  margin-bottom: 10px;
+`;
