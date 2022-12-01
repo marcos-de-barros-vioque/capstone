5314bb56da0de6e424d96b9d3e52810639ac252f
Marcos de Barros Vioque <marcos.debarrosvioque@gmail.com>
2022-12-01 11:48:40 +0100

    adjusted header styling

--- components/Header.js
+++ components/Header.js
@@ -4,20 +4,20 @@ import Link from "next/link";
 
 export function Header() {
     return (
-        <StyledHeader>
-            <StyledLink href="/">
+        <StyledHeaderContainer>
+            <StyledLogo href="/">
               <Image
                 src="/logo_small_icon_only_inverted.png"
                 alt="Spendator app"
-                width="20"
-                height="20"
+                width="50"
+                height="50"
               />
-            </StyledLink>
-        </StyledHeader>
+            </StyledLogo>
+        </StyledHeaderContainer>
     );
 }
 
-const StyledHeader = styled.div`
+const StyledHeaderContainer = styled.div`
   display: flex;
   justify-content: flex-start;
   align-items: left;
@@ -26,6 +26,7 @@ const StyledHeader = styled.div`
   top: 0;
 `;
 
-const StyledLink = styled(Link)`
+const StyledLogo = styled(Link)`
   text-decoration: none;
+  margin: 30px 0px 0px 30px;
 `;
