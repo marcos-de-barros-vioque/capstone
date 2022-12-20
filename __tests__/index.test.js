a1ab8e261bef1d8c5154728fcf34886361d891d1
Marcos de Barros Vioque <marcos.debarrosvioque@gmail.com>
2022-12-20 16:39:13 +0100

    fixed npm run test error

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,6 +1,8 @@
 import {render, screen} from "@testing-library/react";
 import {Layout} from "../components/Layout";
 import "@testing-library/jest-dom";
+jest.mock("next/router", () => require("next-router-mock"));
+jest.mock("next/dist/client/router", () => require("next-router-mock"));
 
 describe("Home", () => {
   it("checks position of navigation", () => {
