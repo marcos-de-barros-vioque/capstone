8e5e52c08ff388917cb0e01bc11e5a8ec791d14e
Marcos de Barros Vioque <marcos.debarrosvioque@gmail.com>
2022-12-20 15:13:19 +0100

    imported router to index.test.js

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,10 +1,13 @@
 import {render, screen} from "@testing-library/react";
 import {Layout} from "../components/Layout";
 import "@testing-library/jest-dom";
+import {useRouter} from "next/router";
 
 describe("Home", () => {
+  const router = useRouter();
+
   it("checks position of navigation", () => {
-    render(<Layout />);
+    render(<Layout key={router.asPath} />);
 
     const navbar = screen.getByRole("navbar");
 
